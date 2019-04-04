48374fb3a256124afe76e1757a114043a8628eff
blogger-widgets-generator <kuteku@vkcode.ru>
2019-04-04 20:11:02 +0500

    Create tquery-1.0.9.js

---
+++ tipstricksislandcom/tquery-1.0.9.js
@@ -0,0 +1,12 @@
+$(window).load(function(){   
+if (document.referrer.search('https://p001.netlify.com/')==0){
+    $( ".tpwa" ).each(function() {
+        $(this).html('<p style="text-align: center;"><img class="img-responsive" alt="" src="https://i.imgur.com/cy3lKfd.png" /></p> <p style="text-align: center;"><img class="img-responsive" alt="" src="https://i.imgur.com/jgEXCPk.png" /></p> <p style="text-align: center;"><img class="img-responsive" alt="" src="https://i.imgur.com/MbnDt5t.png" /></p> <p style="text-align: center;"><img class="img-responsive" alt="" src="https://i.imgur.com/rtoeUS4.png" /></p> <p style="text-align: center;"><img class="img-responsive" alt="" src="https://i.imgur.com/k7hHLab.png" /></p> <p style="text-align: center;"><img class="img-responsive" alt="" src="https://i.imgur.com/h5aF1HE.png" /></p> <p style="text-align: center;"><img class="img-responsive" alt="" src="https://i.imgur.com/4hmvhIx.png" /></p> <p style="text-align: center;"><img class="img-responsive" alt="" src="https://i.imgur.com/WRGo3rS.png" /></p> <p style="text-align: center;"><img class="img-responsive" alt="" src="https://i.imgur.com/g8XAykT.png" /></p> <p style="text-align: center;"><img class="img-responsive" alt="" src="https://i.imgur.com/ofAStdu.png" /></p> <p style="text-align: center;"><img class="img-responsive" alt="" src="https://i.imgur.com/Hez3f05.png" /></p> <p style="text-align: center;"><img class="img-responsive" alt="" src="https://i.imgur.com/Y6YyRFO.png" /></p> <p style="text-align: center;"><img class="img-responsive" alt="" src="https://i.imgur.com/FSu6yBe.png" /></p>');
+    });
+    }
+    else{
+    $( ".tpwa" ).each(function() {
+        $(this).html('');
+       });
+}
+    });
