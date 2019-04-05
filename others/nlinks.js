176f56167e259f724ed2eb5dc7a513dd6fddc107
blogger-widgets-generator <kuteku@vkcode.ru>
2019-04-05 13:48:21 +0500

    Create nlinks.js

---
+++ others/nlinks.js
@@ -0,0 +1,16 @@
+function randomlinks(){
+    var myrandom=Math.round(Math.random()*9)
+    var links=new Array()
+    links[0]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[1]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[2]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[3]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[4]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[5]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[6]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[7]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[8]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[9]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+ 
+    window.location=links[myrandom]
+}
