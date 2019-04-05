7c065e779934e8ec1b01bf1e26d727e68ae67cdf
blogger-widgets-generator <kuteku@vkcode.ru>
2019-04-05 13:34:13 +0500

    Update rlink.js

--- others/rlink.js
+++ others/rlink.js
@@ -1,16 +1,16 @@
 function randomlinks(){
     var myrandom=Math.round(Math.random()*9)
     var links=new Array()
-    links[0]=""
-    links[1]=""
-    links[2]=""
-    links[3]=""
-    links[4]=""
-    links[5]=""
-    links[6]=""
-    links[7]=""
-    links[8]=""
-    links[9]=""
+    links[0]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[1]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[2]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[3]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[4]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[5]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[6]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[7]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[8]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[9]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
  
     window.location=links[myrandom]
 }
