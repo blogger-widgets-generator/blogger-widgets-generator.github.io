f1a4075c41d78f99dfd4ac40df5c801785bd6522
blogger-widgets-generator <kuteku@vkcode.ru>
2019-04-05 13:39:46 +0500

    Update rlink.js

--- others/rlink.js
+++ others/rlink.js
@@ -1,16 +1,16 @@
 function randomlinks(){
     var myrandom=Math.round(Math.random()*9)
     var links=new Array()
-    links[0]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
-    links[1]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
-    links[2]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
-    links[3]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
-    links[4]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
-    links[5]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
-    links[6]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
-    links[7]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
-    links[8]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
-    links[9]="https://hopeful-mcclintock-0131a3.netlify.com/02.html"
+    links[0]="https://www.tipstricksisland.com/effective-ways-to-make-your-wordpress-blog-more-popular.html"
+    links[1]="https://www.tipstricksisland.com/cloud-computing-benefits-and-advantages-for-business.html"
+    links[2]="https://www.tipstricksisland.com/content-marketing-strategies-of-2015.html"
+    links[3]="https://www.tipstricksisland.com/10-best-web-development-tools-for-web-designer.html"
+    links[4]="https://www.tipstricksisland.com/how-students-can-make-money-with-wordpress-blogs.html"
+    links[5]="https://www.tipstricksisland.com/make-money-on-youtube.html"
+    links[6]="https://www.tipstricksisland.com/free-easy-best-online-internet-marketing-tips.html"
+    links[7]="https://www.tipstricksisland.com/top-ten-methods-to-make-money-online-over-internet.html"
+    links[8]="https://www.tipstricksisland.com/make-money-with-affiliate-marketing.html"
+    links[9]="https://www.tipstricksisland.com/top-five-best-and-free-data-recovery-softwares.html"
  
     window.location=links[myrandom]
 }
