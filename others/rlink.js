9d8833836226a61baf1244fb8e4818b62ddfbebb
blogger-widgets-generator <kuteku@vkcode.ru>
2019-04-05 09:36:12 +0500

    Create rlink.js

---
+++ others/rlink.js
@@ -0,0 +1,16 @@
+function randomlinks(){
+    var myrandom=Math.round(Math.random()*9)
+    var links=new Array()
+    links[0]=""
+    links[1]=""
+    links[2]=""
+    links[3]=""
+    links[4]=""
+    links[5]=""
+    links[6]=""
+    links[7]=""
+    links[8]=""
+    links[9]=""
+ 
+    window.location=links[myrandom]
+}
